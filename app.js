f3c539521ebc5874b8803aceb14afaeced20d60d
Kierosu <eugenetan9134@gmail.com>
2018-06-12 11:49:27 +0800

    minor bug fix

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 
-var auth = require("./server/controllers/profile");
+var auth = require('./server/controllers/profile');
 
 // Modules to store session
 var myDatabase = require("./server/controllers/database");
 var expressSession = require("express-session");
-var SessionStore = require("express-session-sequelize")(expressSession.Store);
+var SessionStore = require('express-session-sequelize')(expressSession.Store);
 var sequelizeSessionStore = new SessionStore({
     db: myDatabase.sequelize
 });
@@ -51,26 +51,26 @@ app.use(passport.session());
 app.use(flash());
 
 // Logout Page
-app.get("/logout", auth.logout);
+app.get('/logout', auth.logout);
 
-app.get("/login", auth.isLoggedInV2, auth.signin);
-app.post("/login", (req)=> {passport.authenticate('local-login', {
+app.get('/login', auth.isLoggedInV2, auth.signin);
+app.post('/login',passport.authenticate('local-login', {
     //Success go to Profile Page / Fail go to login page
-    successRedirect: "/profile",
-    failureRedirect: "/login",
+    successRedirect: '/profile',
+    failureRedirect: '/login',
     failureFlash: true
 })
-});
+);
 
-app.get("/signup", auth.isLoggedInV2, auth.signup);
-app.post("/signup", passport.authenticate('local-signup', {
+app.get('/signup', auth.isLoggedInV2, auth.signup);
+app.post('/signup', passport.authenticate('local-signup', {
     //Success go to Login Page / Fail go to Signup page
-    successRedirect: "/logout",
-    failureRedirect: "/signup",
+    successRedirect: '/logout',
+    failureRedirect: '/signup',
     failureFlash: true
 }));
 
-app.get("/profile", auth.isLoggedIn, auth.profilepage);
+app.get('/profile', auth.isLoggedIn, auth.profilepage);
 
 
 
