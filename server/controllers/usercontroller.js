ec1cec7b7ed8b816045a284a0eb599661ab93122
Shizuko <32918698+AhmadShafie@users.noreply.github.com>
2018-08-05 15:31:05 +0800

    block table

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -1,17 +1,20 @@
 var User = require('../models/user');
 var mydatabase  = require( '../controllers/database');  
 var sequelize  = mydatabase.sequelize; 
-var msg = require('../models/msg');
+var msg = require('../models/msg'); 
+var block = require('../models/block');
  
  
 exports.list  = function(req,res){  
     User.findAll({}).then((Users) => {
-        msg.findAll({}).then((msgs) => {
-            res.render('chatpage', {
-                userList : Users,
-                messageList : msgs,
-                 
-            });
+        msg.findAll({}).then((msgs) => { 
+            block.findAll({}).then((blocks) => {
+                res.render('chatpage', {
+                    userList : Users,
+                    messageList : msgs,
+                    blockList : blocks,
+                }); 
+            })
         })
     })
      
