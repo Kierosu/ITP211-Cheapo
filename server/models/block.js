78fc576f862f0b87cc8b9307584e1c44241ce1ea
Shizuko <32918698+AhmadShafie@users.noreply.github.com>
2018-08-05 15:31:41 +0800

    Create block.js

---
+++ server/models/block.js
@@ -0,0 +1,32 @@
+var myDatabase = require('../controllers/database');
+var sequelize = myDatabase.sequelize;
+var Sequelize = myDatabase.Sequelize;
+
+const Block = sequelize.define('Block', {
+    BlockID:{
+        type:Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    
+    blockedby:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    blockeduser:{
+        type: Sequelize.STRING,
+        dallowNull: false
+    }
+});
+
+Block.sync({force: false, logging: console.log}).then(()=>{
+    console.log("Block table synced");
+    Block.upsert({
+         BlockID : 1 ,
+         msg : "message works!" ,
+         blockedby:  "zuko" ,
+         blockeduser: "appa",
+    });
+});
+
+module.exports = sequelize.model('Block', Block);
